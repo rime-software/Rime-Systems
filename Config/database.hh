1ead3f070157928739dff8482ec417f5058505bb
Benjamin J. Anderson <andeb2804@gmail.com>
2015-09-07 16:02:34 -0400

    Update test database config

--- Config/database.hh
+++ Config/database.hh
@@ -7,7 +7,7 @@
     $cfg->set_model_directory(MODEL_PATH);
     $cfg->set_connections(array(
         'development' => 'mysql://rime:a007mxg999@192.168.1.113/rime_systems',
-        'test' => 'mysql://rime:a007mxg999@192.168.1.113/rime_test',
+        'test' => 'mysql://root@localhost/rime_test',
       )
     );
   });
